715c5f82251670fcfb8b64873abcc48258d0fe29
Surya DaVinci <suryadavinci@gmail.com>
2017-05-15 18:59:08 +0530

    layout in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/help'
+  root to: 'static_pages#home'
 
-  get 'static_pages/about'
+  #get 'static_pages/home'
+  get '/home', to: 'static_pages#home'
+  get '/', to: 'static_pages#home'
+
+
+  #get 'static_pages/help'
+  get '/help', to: 'static_pages#help'
+
+  #get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
 
   get 'static_pages/nothing'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
