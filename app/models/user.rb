1c22c184a1f43ed8f6f977d3fa055df6d03a76dc
Surya DaVinci <suryadavinci@gmail.com>
2017-06-01 18:05:02 +0530

    i feel good

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,9 @@
 #
 
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = \@
+  VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true
-  validates :email, presence: true, length: {maximum: 50} , format: {with: VALID_EMAIL_REGEX}
+  validates :email, presence: true, length: {maximum: 50} , format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  before_save { |user| user.email = email.downcase}
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523094849) do
+ActiveRecord::Schema.define(version: 20170601122756) do
 
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170523094849) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
 end
