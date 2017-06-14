b35cb8a2bea034858b145282bef57d553729551b
Surya DaVinci <suryadavinci@gmail.com>
2017-06-14 17:29:10 +0530

    bcrypt done, edited the source code

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,14 +10,14 @@
 #
 
 class User < ApplicationRecord
-  attr accessible :name, :email, :password, :password_confirmation
-  has secure password
+#  attr_accessor :name, :email, :password, :password_confirmation
+  has_secure_password
+  before_save { |user| user.email = email.downcase}
 
   VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true
   validates :email, presence: true, length: {maximum: 50} , format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: { minimum: 6 }
-  validates :password confirmation, presence: true
-  before_save { |user| user.email = email.downcase}
+  validates :password_confirmation, presence: true
 
 end
